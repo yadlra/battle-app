d612cc3d3097c288c6f4db061611258b1b19517a
yadlra <yadi.sb1@gmail.com>
2019-04-19 18:38:06 +0100

    global variables

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require 'sinatra/base'
+require 'player'
 
 class Battle < Sinatra::Base
   enable :sessions
@@ -9,22 +10,22 @@ class Battle < Sinatra::Base
 
   post '/names' do
     p params
-    session[:player_1_name] = params[:player_1_name]
-    session[:player_2_name] = params[:player_2_name]
+    $player_1_name = Player.new(params[:player_1_name])
+    $player_2_name = Player.new(params[:player_2_name])
     redirect '/play'
   end
 
   get '/play' do
-    @player_1_name = session[:player_1_name]
-    @player_2_name = session[:player_2_name]
+    @player_1_name = $player_1_name.name
+    @player_2_name = $player_2_name.name
     erb(:play)
   end
 
   get '/attack' do
-    @player_1_name = session[:player_1_name]
-    @player_2_name = session[:player_2_name]
+    @player_1_name = $player_1_name.name
+    @player_2_name = $player_2_name.name
     erb :attack
   end
-  
+
   run! if app_file == $0
 end
