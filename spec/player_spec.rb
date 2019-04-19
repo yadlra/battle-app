e0e53098cbdc72e25225c0a2b2f5bae1729edc1d
yadlra <yadi.sb1@gmail.com>
2019-04-19 18:32:32 +0100

    added Player class

---
+++ spec/player_spec.rb
@@ -0,0 +1,9 @@
+require 'player'
+describe Player do
+  subject(:anna) { Player.new('Anna') }
+  describe '#name' do
+    it 'returns the name' do
+      expect(anna.name).to eq 'Anna'
+    end
+  end
+end
